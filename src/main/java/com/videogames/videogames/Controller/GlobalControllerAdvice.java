037fba26aaf2066d2638a516a6f2d48fcc4b73b4
federicomolino <federicomolino450@gmail.com>
2025-10-26 13:46:04 +0100

    Add: Aggiunto controller che permette di verificare tutti parametri senza preoccuparci del redirect

---
+++ src/main/java/com/videogames/videogames/Controller/GlobalControllerAdvice.java
@@ -0,0 +1,34 @@
+package com.videogames.videogames.Controller;
+
+import com.videogames.videogames.Service.ParametroService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+
+    private ParametroService parametroService;
+
+    public GlobalControllerAdvice(ParametroService parametroService){
+        this.parametroService = parametroService;
+    }
+
+    //Grazie a questo metodo, ad ogni controller andiamo a verificare i parametri attivi, in modo da non preoccuparci
+    //per nessun redirect
+    @ModelAttribute("parametri")
+    public Map<String,Boolean> globalParametri(Model model){
+        //Inserire il codice Parametro da ricercare
+        List<String> codiceParametri = List.of(
+                "BARRA_RICERCA",
+                "DETTAGLIO_RICERCA");
+
+        HashMap<String, Boolean> parametri = parametroService.VerificaFlagParametro(codiceParametri);
+        model.addAttribute("parametri", parametri);
+        return parametri;
+    }
+}
--- src/main/java/com/videogames/videogames/Controller/indexController.java
+++ src/main/java/com/videogames/videogames/Controller/indexController.java
@@ -9,37 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.HashMap;
-import java.util.List;
-
 @Controller
 public class indexController {
 
     private GiocoService GiocoService;
     private ParametroRepository parametroRepository;
-    private ParametroService parametroService;
 
     @Autowired
     public indexController(GiocoService giocoService, ParametroRepository parametroRepository,
                            ParametroService parametroService){
         this.GiocoService = giocoService;
         this.parametroRepository = parametroRepository;
-        this.parametroService = parametroService;
     }
 
 
     @GetMapping("/")
     public String indexPage(@RequestParam(name = "titolo", required = false)String titolo,
                             Model model){
-
-        //Inserire il codice Parametro da ricercare
-        List<String> codiceParametri = List.of(
-                "BARRA_RICERCA",
-                "DETTAGLIO_RICERCA");
-
-        HashMap <String, Boolean> parametri = parametroService.VerificaFlagParametro(codiceParametri);
-        model.addAttribute("parametri", parametri);
-
         model.addAttribute("list", GiocoService.showGiochi(titolo));
         return "gioco/index";
     }
